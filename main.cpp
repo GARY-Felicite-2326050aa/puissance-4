d4f8a6ef7eebc405738133d088c56072610a89c0
Félicité Gary <146103677+felicitegary@users.noreply.github.com>
2023-10-16 10:58:23 +0200

    Update PFC main.cpp
    Pierre Feuille Ciseau

--- main.cpp
+++ main.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 void pierreFeuilleC()
 {
-    int j1,j2,p1=0,p2=0;
+    int j1,j2 = 0,p1=0,p2=0;
+
 
     for(int i=1;i<=3;i++)
     {
         cout << "rentrez 1 pour pierre,2 pour feuille,3 pour ciseau \n";
         cin >> j1;
         cin >> j2;
+        while(j1 == j2)
+        {
+            cout << "rentrez 1 pour pierre,2 pour feuille,3 pour ciseau \n";
+            cin >> j1;
+            cin >> j2;
+        }
+
         if((j1==1 && j2 == 2 )|| (j1== 2 && j2 == 3)||(j1==3 && j2 ==1))
         {
             p2 += 1;
@@ -21,6 +29,7 @@ void pierreFeuilleC()
             p1 += 1;
             cout << "point pour le joueur 1"<<endl;
         }
+
     }
     if(p2<p1)
     {
@@ -34,8 +43,6 @@ void pierreFeuilleC()
     {
         cout << "égalité"<<endl;
     }
-
-
 }
 
 int main()
